bc33b06d69bfbbeed4abd46d1a1c9882a4644dce
Guilhermealves20101999 <guiguialves24@gmail.com>
2022-07-27 17:41:13 -0300

    first commit

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { ThemeContext } from "../../ContextProvider/ThemeContext";
+import WindowSize from "../../Utils/WindowSize";
+import styles from "./About.module.css";
+
+
+const About = () => {
+  const { newTheme } = React.useContext(ThemeContext);
+  const [width] = WindowSize();
+  const img =
+    width < 650
+      ? "https://github.com/RahulGoyal03/Portfolio/blob/main/public/IMG_3379-removebg-modified_ccexpress__1_-removebg-preview.png?raw=true"
+      : "https://github.com/RahulGoyal03/Portfolio/blob/main/public/developer.gif?raw=true";
+  return (
+    <div
+      className={styles.container}
+      style={{ boxShadow: `3px 3px 5px ${newTheme.line}` }}
+    >
+      <div className={styles.first}>
+        <img
+          src={
+            "https://github.com/RahulGoyal03/Portfolio/blob/main/public/developer.gif?raw=true"
+          }
+          alt=""
+        />
+      </div>
+      <div className={styles.second}>
+        <h1 style={{ color: `${newTheme.title}` }} className={styles.heading}>
+          Sobre 
+        </h1>
+        <div className={styles.borderBottom} />
+        <p style={{ color: `${newTheme.para}` }} className={styles.aboutMe}>
+          Olá! Meu nome é Guilherme Alves e eu gosto de criar coisas que vivem
+          a Internet. Meu interesse em desenvolvimento Front-End começou em 2019
+          quando eu estava tentando editar coisas na web - me ensinou muito sobre
+          HTML e CSS!. <br />
+          <br /> Avançando para hoje, criei vários aplicativos da web
+          e grandes projetos. Aprendi muito sobre trabalho em equipe, liderança,
+          e comunicação. Após anos de treinamento rigoroso, aqui estou
+          <span style={{ color: `#00a0a0` }}>
+            {" "}
+            procurando uma oportunidade como desenvolvedor Front-end
+          </span>
+          .
+        </p>
+      </div>
+      
+    </div>
+  );
+};
+
+export default About;
---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { ThemeContext } from "../../ContextProvider/ThemeContext";
+import styles from "./Contact.module.css";
+
+const Contact = () => {
+  const { newTheme } = React.useContext(ThemeContext);
+  return (
+    <footer
+      className={styles.footer}
+      id="contact"
+      // style={{ background: `${newTheme.highlightBackground}` }}
+    >
+      <div className={styles.container}>
+        <h1 style={{ color: `${newTheme.title}` }}>Entrar em contato</h1>
+        <p style={{ color: `${newTheme.para}` }} className={styles.paragraph}>
+        Estou procurando ativamente novas oportunidades, em Desenvolvedor front-end</p>
+    
+          <p style={{ color: `#00a0a0`, lineHeight: ``,fontSize:`20px` }}>
+            {/* {" "} */}
+            Email: guilhermealvesmor@gmail.com
+          </p>
+
+		  <p style={{ color: `#00a0a0`,fontSize:`20px`}}>
+		  Mobile: +55-<span style={{ color: `#00a0a0`, fontSize:`20px`, textDecoration: `underline` }} >(61) 99397-9955</span>{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}
+          </p>
+		  <p style={{ color: `#00a0a0`,fontSize:`20px` }}>
+            Endereço: Brasilia/DF, Brasil{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}
+          </p>
+		  
+        <div
+          style={{ color: `${newTheme.title}` }}
+          className={styles.contactOptions}
+        >
+          <a
+            href="tel:7420959650"
+            aria-label="GitHub"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <i className="fas fa-phone-alt" />
+          </a>
+          <a
+            href="https://mail.google.com/mail/u/0/#inbox"
+            aria-label="email"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="far fa-envelope"></i>
+          </a>
+          <a
+            href="https://twitter.com/giri_sayli"
+            aria-label="Twitter"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a
+            href="https://github.com/Sayli555"
+            aria-label="GitHub"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <i className="fab fa-github" />
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/sayli-giri/"
+            aria-label="Linkedin"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fab fa-linkedin-in" />
+          </a>
+        </div>
+      </div>
+      <div style={{ background: `${newTheme.line}` }} className={styles.line} />
+      <div style={{ color: `${newTheme.para}` }} className={styles.copyright}>
+      Projetado e construído por Guilherme Alves, 2022 Todos os direitos reservados
+      </div>
+    </footer>
+  );
+};
+
+export default Contact;
