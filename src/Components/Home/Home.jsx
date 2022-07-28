e09559f8a2d176f7404f6f23cf65d990576ac650
Guilhermealves20101999 <guiguialves24@gmail.com>
2022-07-28 12:42:18 -0300

    Foto

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,7 +5,7 @@ import SideIcons from "../SideIcons/SideIcons";
 import TechStacks from "../TechStacks/TechStacks";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import img1 from "../../assets/my_photo.jpg";
+import img1 from "../../assets/Gui.png";
 import { Button } from "../Button/Button";
 import About from "../About/About";
 import { projects } from "../../Utils/Projects";
